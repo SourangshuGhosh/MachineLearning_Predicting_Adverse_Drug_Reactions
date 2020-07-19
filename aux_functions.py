7cb840c0df6a24b848f0d52e2ab607c0b82b341f
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-19 18:07:04 +0530

    Converting Numpy Array to List

---
+++ aux_functions.py
@@ -0,0 +1,11 @@
+import numpy as np
+from rdkit import DataStructs
+
+
+def to_numpyarray_to_list(desc):
+    arr = np.zeros((1,))
+    DataStructs.ConvertToNumpyArray(desc, arr)
+    return arr.tolist()
+
+
+
